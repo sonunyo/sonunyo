038fd1ce0aa08bd543879b159772a91b9815d4b0
sonunyo <sonu.v.s78@gmail.com>
2025-03-29 05:31:26 +0530

    Update New code

---
+++ src/Demo03printf.java
@@ -0,0 +1,13 @@
+public class Demo03printf {
+//printf -formatting(it will replace the value in the %)
+    public static void main(String[] args) {
+        int a=10;
+        System.out.printf("your varriable name is %d",a);
+        //%d-int, byte, long, short, data types
+        //%s for string data dtypes
+        //%f for float data types
+        //%b for boolean data types
+    }
+
+
+}
